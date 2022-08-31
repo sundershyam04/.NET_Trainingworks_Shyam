04c41b4994a6fdf676aeac646c27a7931e53962f
sundershyam04 <60753448+sundershyam04@users.noreply.github.com>
2022-08-31 15:25:41 +0530

    Add files via upload

---
+++ 23Aug_Structures/prob1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prob1
+{
+    internal class Program
+    {  static void add(int a, int b)
+        {
+            Console.WriteLine($"Addition :{a+b}");
+        }
+           
+        static void Main(string[] args)
+        {
+            int a,b;
+            Console.WriteLine("Enter a:");
+            a=int.Parse(Console.ReadLine());   
+            Console.WriteLine("Enter b:");
+            b=int.Parse(Console.ReadLine());
+            add(a,b);   // func call
+            Console.Read();
+         }
+    }
+}
