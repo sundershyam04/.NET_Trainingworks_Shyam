0d5faa637eabfca63680110b32a079732d6000d5
sundershyam04 <60753448+sundershyam04@users.noreply.github.com>
2022-08-31 13:39:58 +0530

    Add files via upload

---
+++ 19Aug-Basics/prob1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prob1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse (Console.ReadLine());
+            int b = int.Parse (Console.ReadLine());
+
+            if (a > b)
+            {
+                Console.WriteLine($"Maximum Number : {a}");
+                Console.WriteLine($"Minimum Number : {b}");
+            }
+            else
+            {
+                Console.WriteLine($"Maximum Number : {b}");
+                Console.WriteLine($"Minimum Number : {a}");
+            }
+        }
+    }
+}
