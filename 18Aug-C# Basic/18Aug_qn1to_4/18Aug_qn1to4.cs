efffbd372e8f9bb4f5438c30751f64586bf97396
sundershyam04 <60753448+sundershyam04@users.noreply.github.com>
2022-08-31 13:35:59 +0530

    Add files via upload

---
+++ 18Aug-C# Basic/18Aug_qn1to_4/18Aug_qn1to4.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_basics_18Aug_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b;
+            Console.WriteLine("enter a:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter b:");
+            b= Convert.ToInt32(Console.ReadLine());
+            //call by value
+             void swapval(int x, int y)
+            {
+                int temp = x;
+                x = y; y = temp;
+                
+            }
+            //call by reference
+
+            void swapref(ref int x, ref int y)
+            {
+                int temp = x;
+                x = y;
+                y = temp;
+            }
+            swapval(a, b);
+            Console.WriteLine($"Swapbyvalue: \n a :{a}   b : {b}");
+            
+            swapref(ref a, ref b);
+            Console.WriteLine($"Swapbyreference \n a:{a}   b : {b}");
+            Console.WriteLine("-------------------------------------------");
+
+            //qn2- MUltiplication of 3 numbers
+            int p, q, r,s;
+            Console.WriteLine("enter p:");
+            p = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter q:");
+            q = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter r:");
+            r = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter s:");
+            s = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{p} * {q} * {r} = {p*q*r}");
+            Console.WriteLine("-------------------------------------------");
+
+            //Qn3- arithmetic operation
+
+            Console.WriteLine($"Addition : {p + q + r}");
+            Console.WriteLine($"Subtraction : {p - q - r}");
+            Console.WriteLine($"Division of p by q : {p/q}");
+
+            //qn4 -Average of 4 nos
+            Console.WriteLine($"Average of p q r s:  {((p + q + r + s)/ 4)}");
+
+        }
+    }
+}
---
+++ 18Aug-C# Basic/18Aug_qn5to_9/18Aug_qn5to_9.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18Aug_qn5to_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //5 - Arith opr
+            int x, y, z;
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            z = Convert.ToInt32(Console.ReadLine());
+
+            int res1 = (x + y) * z;
+            int res2 = (x * y) + (y * z);
+            Console.WriteLine($"(x + y) * z : {res1}");
+            Console.WriteLine($"(x * y) + (y * z) : {res2}");
+            //6
+            int age;
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"You look older than {age}");
+
+           // 7
+            int num;
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (int j = 0; j < 4; j++)
+            {
+                for (int k = 0; k < 4; k++)
+                {
+                    Console.Write(num);
+                    if (j % 2 == 0)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine('\n');
+
+            }
+
+            //8
+            Console.WriteLine("rectangle pattern");
+            int row, col;
+
+            row = Convert.ToInt32(Console.ReadLine());
+            col = Convert.ToInt32(Console.ReadLine());
+
+            void rect(int m, int n)
+            {
+                for (int k = 0; k < m; k++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        if ((k != (m - 1) && k != 0) && (j == 1))
+                        {
+                            Console.Write(" ");
+                            continue;
+                        }
+                        Console.Write(num);
+                    }
+                    Console.WriteLine('\n');
+                }
+
+            }
+
+            rect(row, col);
+
+            //9
+            Console.Write("Enter the Temperature :");
+            double temperature = Convert.ToSingle(Console.ReadLine());
+
+            double kelvin = temperature + 273.15;
+
+            double Fah = (temperature * 9 / 5) + 32;
+
+            Console.WriteLine($"Celsius : {temperature}");
+            Console.WriteLine($"Kelvin : {kelvin}");
+            Console.WriteLine($"Fahrenheit : {Fah}");
+
+          
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
