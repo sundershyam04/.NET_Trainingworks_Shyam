01edcf304ce185b4d870264af0dec605e315318e
sundershyam04 <60753448+sundershyam04@users.noreply.github.com>
2022-08-31 13:37:04 +0530

    Add files via upload

---
+++ 18Aug-C# Basic/prob10/prob10.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prob10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter n1 and n2");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+             
+            bool res= (n1>0 && n2<0 || n1<0 && n2>0);
+
+            Console.WriteLine(res);
+            
+
+        }
+    }
+}
---
+++ 18Aug-C# Basic/prob12/prob12.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prob12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string");
+            string str = Console.ReadLine();
+
+            Console.WriteLine($"{str} is null??");
+            Console.WriteLine(String.IsNullOrEmpty(str));
+        }
+    }
+}
