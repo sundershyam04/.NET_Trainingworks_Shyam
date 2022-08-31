32d174fecaa41469bdc7988383ab02e2637b3257
sundershyam04 <60753448+sundershyam04@users.noreply.github.com>
2022-08-31 15:46:19 +0530

    Add files via upload

---
+++ 24Aug_DateTime_StrBuilder/logicofapp/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logicofapp
+{
+    public class Class1
+    {
+
+        public static void Hello()
+        {
+            Console.WriteLine("Hello from class library \t(logic of app project)");
+        }
+    }
+}
---
+++ 24Aug_DateTime_StrBuilder/logicofapp/Filemethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace logicofapp
+{
+    public class Filemethods
+    {
+        public static void makefile()
+        {
+            string p = "C:\\Users\\SHYAM SUNDER\\Desktop\\Todo\\dem.txt";
+            File.Create(p);
+            Console.WriteLine("file created");
+        }
+    }
+}
