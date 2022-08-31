5eadb107c04bd18845077d4f92dfc498442d8c57
sundershyam04 <60753448+sundershyam04@users.noreply.github.com>
2022-08-31 13:33:41 +0530

    Add files via upload

---
+++ 16Aug-Basics, Statements/16Aug_basics.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Multiplication of two numbers
+            int a = 50;
+            int b = 5;
+            int c = a * b;
+            Console.WriteLine("a*b : {0}",c);
+            Console.WriteLine("--------------------------");
+
+            // string inputs
+            Console.Write("Enter username : ");
+            string username=Console.ReadLine();
+
+            Console.Write("Enter password : ");
+            string password=Console.ReadLine();
+
+            Console.WriteLine("Username : {0}\nPassword : {1}",username,password);
+            Console.WriteLine("--------------------------");
+
+            //ifelse 
+
+            Console.Write("Enter age : ");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("-----");
+
+            if (age >= 18)
+                Console.WriteLine("Major");
+            else
+                Console.WriteLine("Minor");
+
+            Console.WriteLine("-----");
+        }
+    }
+}
