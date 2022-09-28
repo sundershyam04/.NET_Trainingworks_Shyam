9cffa7fd0d54df171189c90ac528f64feb8caaa5
sundershyam04 <60753448+sundershyam04@users.noreply.github.com>
2022-09-28 21:32:21 +0530

    Entity Framework
    DB first  approach

---
+++ 28Sept - Entity Framework/demo/EF_DB_Shoppingdemo/BusinessLogicLibraryShopping/CustomerBAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLibraryShopping
+{
+    //Validation-Insert/Updating/Deleting
+    public class CustomerBAL
+    {
+        public int custid { get; set; }
+        public string customerName { get; set; }
+        public string customerCity { get; set; }
+    }
+}
