10d4574c6c4d292dd90ba5b9e8d34efb303ba880
sundershyam04 <60753448+sundershyam04@users.noreply.github.com>
2022-10-01 13:24:48 +0530

    EF - Code First Approach

---
+++ 29Sept - Code1st/BankingCode1st/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankingDB;
+
+namespace BankingCode1st
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Banking  context = new Banking();
+
+            //create borrower and loan
+
+            Borrower b = new Borrower();
+
+            b.BorrowerId = 2312;
+            b.Name = "Siva";
+            b.City = "bangalore";
+            b.State = "TamilNadu";
+            b.Country = "India";
+            b.StreetName = "anna street";
+            b.CompanyName = "dell";
+            b.JobName = "Cloud Architect";
+
+     
+            Loan l = new Loan();
+
+            l.LoanId = 7;
+            l.LoanType = "Personal";
+            l.LoanAmount = 2507000.75f;
+            l.Durationmonths = 36;
+            l.InterestRate = 4.5f;
+
+            context.Loans.Add(l);
+
+            Loan l1 = new Loan();
+
+            l1.LoanId = 8;
+            l1.LoanType = "Housing";
+            l1.LoanAmount = 1000070.75f;
+            l1.Durationmonths = 24;
+            l1.InterestRate = 3.5f;
+
+            context.Loans.Add(l1);
+
+
+            List<Loan> llist = new List<Loan>();
+            llist.Add(l);
+            llist.Add(l1);
+
+            b.loanlist = llist;
+
+          
+
+            context.Borrowers.Add(b);
+
+           
+
+            context.SaveChanges();
+
+            Console.WriteLine(  "Successfull...");
+
+            Console.Read();
+
+        }
+
+    }
+}
---
+++ 29Sept - Code1st/BankingDB/BankDB.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Runtime.Remoting.Contexts;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankingDB
+{
+
+    public class Loan
+    {
+        [Key, Required]
+        public int LoanId { get; set; }
+        public string LoanType { get; set; }
+
+        public Single LoanAmount { get; set; }
+
+        public int Durationmonths { get; set; }
+
+        public Single InterestRate { get; set; }
+
+        public virtual List<Borrower> borrlist { get; set; }
+
+    }
+    public class Borrower
+    {
+        [Key]
+        [Required]
+        public int BorrowerId { get; set; }
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Country { get; set; }
+        [DataType(DataType.Text, ErrorMessage = "Street name Should be a text")]
+        public string StreetName { get; set; }
+      
+        public string CompanyName { get; set; }
+
+        public string JobName { get; set; }
+
+        [ForeignKey("LoanId")]
+        public virtual List<Loan> loanlist {  get; set; }
+
+
+    }
+
+    
+
+
+    public class Banking : DbContext
+    {
+        public Banking(): base("Banking")
+        {
+            Database.SetInitializer<Banking>(new CreateDatabaseIfNotExists<Banking>());
+        }
+
+        public virtual DbSet<Borrower> Borrowers { get; set; }
+        public virtual DbSet<Loan> Loans { get; set; }
+    }
+
+}
