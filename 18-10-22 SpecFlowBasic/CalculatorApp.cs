b324baa12cae75e9a0444cd64badb60e8161622a
sundershyam04 <60753448+sundershyam04@users.noreply.github.com>
2022-10-18 16:20:01 +0530

    specflow intro

---
+++ 18-10-22 SpecFlowBasic/CalculatorApp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecFlowBasic
+{
+    public class CalculatorApp
+    {
+
+        public int Add(int a,int b)
+        {
+
+            return a + b;
+        }
+
+        public int Subtract(int a, int b)
+        {
+
+            return a - b;
+        }
+
+        public int Multiply(int a, int b)
+        {
+
+            return a * b;
+        }
+
+        public int Divide(int a, int b)
+        {
+
+            return b != 0 ? a / b : 0;
+        }
+
+        public double Sqrroot(double a)
+        {
+
+            return a!=0 ? Math.Sqrt(a) : 0;
+        }
+    }
+}
