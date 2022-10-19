40754c18de988cab11aba3939ed6008f22f9e1e5
sundershyam04 <60753448+sundershyam04@users.noreply.github.com>
2022-10-19 17:19:55 +0530

    Unit Testing - NUnit
    LibraryDB

---
+++ 19-10-22 UnitTesting-LibraryDB/LibraryTest/TestingHandle.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestingHandson;
+
+namespace LibraryTest
+{
+    [TestFixture]
+    public class TestingHandle
+    {
+
+        [Test]
+        [SetUp]
+
+        public void GetBookName()
+        {
+
+            LibraryDBoperations opr = new LibraryDBoperations();
+
+            string bname =opr.GetBookName();
+
+            Assert.AreEqual("Attitude is everything", bname, "Book not found....");
+
+        }
+
+        [Test]
+
+        public void GetBookAuthor()
+        {
+
+            LibraryDBoperations opr = new LibraryDBoperations();
+
+            string Aname = opr.GetBookAuthor();
+
+            Assert.AreEqual("jeff keller                   ", Aname, "Author not found....");
+
+        }
+    }
+}
---
+++ 19-10-22 UnitTesting-LibraryDB/UnitTestingHandson/LibraryDBoperations.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingHandson
+{
+    public class LibraryDBoperations
+    {
+        SqlConnection cn;
+        SqlCommand cmd;
+
+        //public LibraryDBoperations()
+        //{
+             
+        //}
+
+        public string GetBookName()
+        {
+            cn = new SqlConnection("Data Source=DESKTOP-45VNN8N;Initial Catalog=Lib;Integrated Security=True");
+
+            cmd = new SqlCommand("select Book_Name from LibBook where" +
+               " Book_No= 871", cn);
+
+            cn.Open();
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while(dr.Read())
+            {
+                return dr[0].ToString();
+            }
+
+            cn.Close();
+            
+
+            return null;
+        }
+
+        public string GetBookAuthor()
+        {
+            cn = new SqlConnection("Data Source=DESKTOP-45VNN8N;Initial Catalog=Lib;Integrated Security=True");
+
+            cmd = new SqlCommand("select Author from LibBook where" +
+               " Book_No= 871", cn);
+
+            cn.Open();
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            while (dr.Read())
+            {
+                return dr[0].ToString();
+            }
+
+            cn.Close();
+
+
+            return null;
+        }
+
+
+
+
+    }
+}
